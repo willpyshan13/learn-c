2908f3ea5dc00d1922ecf4c60a909a4d9b1c31a3
will <545512533@qq.com>
2021-06-17 14:49:45 +0800

    指针常量和常量指针

--- main7.c
+++ main7.c
@@ -10,7 +10,39 @@ void test01(){
 
     char a = 'a';
     char *b = &a;
-    printf("%p \n", sizeof(b));
+//    printf("%p \n", sizeof(b));
+    char *p = 'a';
+    char *name = &p;
+//    printf("%c\n",name);
+
+    //===========
+    const int *c = 111;
+    //常量指针
+    // 指针和 const 谁在前先读谁 ；
+    //*象征着地址，const象征着内容；
+    //谁在前面谁就不允许改变。
+    const int *c1;
+    int const *c2;
+    //指针常量
+    int *const c3;
+
+    int m = 2;
+    int const * m1 = &m;
+//    *m1 = 333;
+
+//    int n = 2;
+//    int n1 = 3;
+//    int const *n2 = &n;
+//    printf("%p \n",n2);
+//    n2 = &n1;
+//    printf("%p \n",n2);
+
+    int n = 2;
+    int n1 = 3;
+    int  *const n2 = &n;
+    printf("%d \n",*n2);
+    *n2 = 4;
+    printf("%d \n",*n2);
 }
 
 int main(){
