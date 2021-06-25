fb4d4c661592248dafad92b69abf5e8a329279cf
will <545512533@qq.com>
2021-06-25 10:48:36 +0800

    添加每日一题的解法20210625

--- main5.c
+++ main5.c
@@ -5,7 +5,7 @@
 #include "stdint.h"
 
 void test01(){
-    int a[] = {112,22,33};
+    int a[] = {112,22,33,22};
     int b[] = {11};
 
     int * array[2] = {a,b};
@@ -13,6 +13,20 @@ void test01(){
     printf("%d \n",**array);
 
     printf("%d \n",*(*(array)+1));
+
+    int *p1 = NULL;
+
+    int a1 = 10;
+    char c = "c";
+    printf("%d \n",sizeof a1);
+    printf("%d \n",sizeof(c));
+    printf("%d \n",sizeof(a));
+
+    printf("%d \n",a1);
+    int *p = &a1;
+    *p = 33;
+
+    printf("%d",a1);
 }
 
 int main(){
