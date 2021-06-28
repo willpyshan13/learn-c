ea60d864df36b649c9dfb46fdd53a8025bf2a3e8
will <545512533@qq.com>
2021-06-28 09:32:21 +0800

    add file

---
+++ main8.c
@@ -0,0 +1,17 @@
+//
+// Created by will on 2021/6/9.
+//
+#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void test01(){
+    int *p;
+    printf("---%p  ",p);
+    int * aar = malloc(16);
+}
+
+int main(){
+    test01();
+    return 0;
+}
