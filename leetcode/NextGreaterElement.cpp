c35daaee17970a8eabcb28987d9555fa081e7bb6
will <545512533@qq.com>
2021-10-26 09:44:41 +0800

    力扣日常2021.10.26

---
+++ leetcode/NextGreaterElement.cpp
@@ -0,0 +1,23 @@
+//
+// Created by will on 2021/10/26.
+//
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> hashmap;
+        stack<int> st;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int num = nums2[i];
+            while (!st.empty() && num >= st.top()) {
+                st.pop();
+            }
+            hashmap[num] = st.empty() ? -1 : st.top();
+            st.push(num);
+        }
+        vector<int> res(nums1.size());
+        for (int i = 0; i < nums1.size(); ++i) {
+            res[i] = hashmap[nums1[i]];
+        }
+        return res;
+    }
+};
